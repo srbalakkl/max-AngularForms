9015ddfd2592f38dc507ea7b0611d765916c9b1b
sr.bala <sr.bala.kkl@gmail.com>
2021-10-05 19:00:26 +0530

    initial commit

---
+++ src/app/angform/angform.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: 'app-angform',
+  templateUrl: './angform.component.html',
+  styleUrls: ['./angform.component.css']
+})
+export class AngformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("it's running");
+  }
+
+  OnSubmit(form : NgForm){
+    console.log('submitted');
+    console.log(form);
+  }
+
+}
