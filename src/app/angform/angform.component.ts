8b943fd73439b106936372a387fe913ad8434ed0
sr.bala <sr.bala.kkl@gmail.com>
2022-01-05 14:54:12 +0530

    last commit

--- src/app/angform/angform.component.ts
+++ src/app/angform/angform.component.ts
@@ -1,26 +1,91 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from "@angular/forms";
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-angform',
-  templateUrl: './angform.component.html',
-  styleUrls: ['./angform.component.css']
+    selector: 'app-angform',
+    templateUrl: './angform.component.html',
+    styleUrls: ['./angform.component.css']
 })
+
+
 export class AngformComponent implements OnInit {
+    afp1='';
 
-  constructor() { }
-  answer = '';
+    // constructor(public dialog: MatDialog) {
+    constructor() {
+    }
 
-  defaultQuestion = 'teacher';
-  gender =['male','female'];
+    // @ViewChild('ds') signupForm: NgForm;
 
-  ngOnInit(): void {
-    console.log("it's running");
-  }
+    //view child is the decorator
+    //@ViewChild('f') -> ViewChild Decorator with template reference variable
 
-  OnSubmit(form :NgForm){
-    console.log('submitted');
-    console.log(form);
-  }
+    answer = '';
+    defaultQuestion = 'teacher';
+    gender = ['male', 'female'];
+    user = {
+        username: '',
+        email: '',
+        secret: '',
+        gender: ''
+    }
+    defUserName = "bala"
 
+    ngOnInit(): void {
+        console.log("it's running");
+    }
+
+    // OnSubmit(form: NgForm) {
+    //     console.log('submitted');
+    //     console.log(form);
+    //     // this.user.username = this.signupForm.value;
+    //     console.log(this.user.username);
+    // }
+
+    OnSubmit(form: NgForm) {
+        console.log('submitted');
+        console.log(form);
+        // this.user.username = this.signupForm.value;
+        console.log(this.user.username);
+
+    }
+
+    suggestUserName() {
+        console.log("suggest user name is working");
+        const username = "SupperUser";
+        // this.signupForm?.setValue({
+        //   userData:{
+        //     username : username,
+        //     email:'',
+        //   },
+        //   secret:'',
+        //   gender:'mala'
+        //
+        // });
+
+
+        // this.signupForm.form.patchValue({
+        //     userData:{
+        //       username : username,
+        //       email:'',
+        //     },
+        //     secret:'',
+        //     gender:'male'
+        // })
+    }
+
+//     openPredictionImg() {
+//         const myImg = new Image();
+//         myImg.useMap = './../assets/WhatsApp Image 2021-11-15 at 3.50.33 PM (1).jpeg';
+//         const img = document.createElement('img');
+//         document.body.appendChild(img);
+//         img.setAttribute('src', myImg.useMap);
+//         img.onload = (stop);
+//
+//         console.log("asdf");
+//     }
+    mtd() {
+        console.log(this.afp1)
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule, ViewChild, ViewChildren} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AngformComponent } from './angform/angform.component';
 import {FormsModule} from "@angular/forms";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NoopAnimationsModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
